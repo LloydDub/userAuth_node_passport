df4c13b26cd3c2df994af303ed09933fed741f27
LloydDub <lloydrww@gmail.com>
2022-03-13 17:27:36 -0600

    Random commit to make me look good

--- server.js
+++ server.js
@@ -1,11 +1,20 @@
 const express = require("express");
-
 const app = express();
 
 app.set("view engine", "ejs");
 
-app.listen(3000);
-
 app.get("/", (req, res) => {
-  res.render("index.ejs");
+  res.render("index.ejs", { name: "Bob" });
 });
+
+app.get("/login", (req, res) => {
+  res.render("login.ejs");
+});
+
+app.get("/register", (req, res) => {
+  res.render("register.ejs");
+});
+
+app.post("/register", (req, res) => {});
+
+app.listen(3000);
