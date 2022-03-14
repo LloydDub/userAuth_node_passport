7af736436e70122cb5b737a7fa11b3685ff702d5
LloydDub <lloydrww@gmail.com>
2022-03-13 19:57:40 -0600

    fixed error with id not defined

--- passport-config.js
+++ passport-config.js
@@ -1,19 +1,18 @@
 const LocalStrategy = require("passport-local").Strategy;
 const bcrypt = require("bcrypt");
-const { use } = require("passport/lib");
 
 function initialize(passport, getUserByEmail, getUserById) {
   const authenticateUser = async (email, password, done) => {
     const user = getUserByEmail(email);
-    if (user === null) {
-      return done(null, false, { message: "Invalid email" });
+    if (user == null) {
+      return done(null, false, { message: "No user with that email" });
     }
 
     try {
       if (await bcrypt.compare(password, user.password)) {
         return done(null, user);
       } else {
-        return done(null, false, { message: "Password Invalid" });
+        return done(null, false, { message: "Password incorrect" });
       }
     } catch (e) {
       return done(e);
@@ -22,7 +21,7 @@ function initialize(passport, getUserByEmail, getUserById) {
 
   passport.use(new LocalStrategy({ usernameField: "email" }, authenticateUser));
   passport.serializeUser((user, done) => done(null, user.id));
-  passport.deserializeUser((user, done) => {
+  passport.deserializeUser((id, done) => {
     return done(null, getUserById(id));
   });
 }
